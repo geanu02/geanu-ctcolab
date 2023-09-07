7a04df605b7959a0508a13e284e3034a22d0dbd1
Gian Carlo Torres <geanu@outlook.com>
2023-09-07 00:22:20 -0400

    FooterLeft message revised

--- src/FooterLeft.tsx
+++ src/FooterLeft.tsx
@@ -1,7 +1,7 @@
 export default function FooterLeft() {
   return (
     <>
-        <p className="fl-body">I found these programming <a href="https://sv443.net/jokeapi/v2/">Jokes API</a> that changes every time you refresh this page. My partner often reminds me to enjoy the small things in life because it helps brighten up my day, even in the darkest of days!
+        <p className="fl-body">I found these programming <a href="https://sv443.net/jokeapi/v2/">Jokes API</a> that changes every time you refresh this page. My partner often reminds me to find ways to laugh everyday because it helps brighten up even in the darkest of days!
         </p>
     </>
   )
